3d7d02728bccdad12fcf64adb5d5804203180bf7
andretex <75755801+andretec16@users.noreply.github.com>
2022-03-06 14:19:04 +0100

    update

--- src/main/java/me/andretex/Main.java
+++ src/main/java/me/andretex/Main.java
@@ -8,44 +8,7 @@ import net.md_5.bungee.config.Configuration;
 import java.sql.SQLException;
 
 public final class Main extends Plugin {
-/*
-    private static Main istance;
-    public Configuration config;
 
-    public MySQL SQL;
-
-    public Main() {
-    }
-
-    @Override
-    public void onEnable() {
-        instance = this;
-        this.config = (new Config()).getConfig();
-
-        this.SQL = new MySQL();
-
-        try {
-            SQL.connect();
-        } catch (ClassNotFoundException | SQLException e) {
-
-            System.out.println("Database not connected");
-        }
-
-        if (SQL.isConnected()) {
-            System.out.println("Database connected");
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    public static Main getInstance() {
-        return instance;
-    }
-
- */
 
     private static Main instance;
     public Configuration config;
@@ -79,22 +42,7 @@ public final class Main extends Plugin {
         this.getLogger().info(Message.color("\n &6EasyLogin &cDISABLE &r\n"));
     }
 
-    /*public void getCommands() {
-        String ssPerm = this.config.getString("commands.ss.permission");
-        String fnishPerm = this.config.getString("commands.finish.permission");
-        if (ssPerm != null) {
-            this.getProxy().getPluginManager().registerCommand(this, new SSCommand("ss", ssPerm));
-        } else {
-            this.getProxy().getPluginManager().registerCommand(this, new SSCommand("ss"));
-        }
 
-        if (fnishPerm != null) {
-            this.getProxy().getPluginManager().registerCommand(this, new FinishCommand("finish", fnishPerm));
-        } else {
-            this.getProxy().getPluginManager().registerCommand(this, new FinishCommand("finish"));
-        }
-
-    }*/
 
     public static Main getInstance() {
         return instance;
